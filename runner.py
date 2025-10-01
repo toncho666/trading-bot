20b6192c765c2e0f277af528b3f4b2e0e56272b4
toncho666 <antonbaburkin@mail.ru>
2025-10-02 00:04:48 +0300

    add requirements and runner

--- runner.py
+++ runner.py
@@ -9,6 +9,14 @@ DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 
+print("Environment variables:")
+print(f"DB_HOST: {os.getenv('DB_HOST')}")
+print(f"DB_NAME: {os.getenv('DB_NAME')}")
+print(f"DB_USER: {os.getenv('DB_USER')}")
+print(f"DB_PASS: {os.getenv('DB_PASS')}")
+
+
+
 # Создаём клиент биржи (Binance через ccxt)
 exchange = ccxt.binance()
 
