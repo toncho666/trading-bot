285c222c409424cb63e2dde439fe8127851db9cd
toncho666 <38908277+toncho666@users.noreply.github.com>
2025-10-04 16:32:47 +0300

    Update runner.py

--- runner.py
+++ runner.py
@@ -54,9 +54,12 @@ def run_strategy(file):
         # сохраняем весь датафрейм в отдельную таблицу
         strategy_name = os.path.splitext(os.path.basename(file))[0]
         table_name = f"signal_df_{strategy_name}"
-        table_schema = 'test'
 
-        signal_df.to_sql(table_name, table_schema, engine, if_exists="replace", index=True)
+        signal_df.to_sql(name=table_name
+                        ,schema='test'
+                        ,con=engine
+                        ,if_exists="replace"
+                        ,index=True)
         print(f"DataFrame сохранён в таблицу {table_name}")
         
         # берём последнюю строку
