a68a291d1b2e931872d5579a7ad8ddb90867b01a
toncho666 <38908277+toncho666@users.noreply.github.com>
2025-10-04 16:14:00 +0300

    Update hist_data.py

--- hist_data.py
+++ hist_data.py
@@ -1,7 +1,7 @@
 # --- Получение исторических данных ---
 def fetch_data(symbol="BTC/USDT", timeframe="1h", limit=200):
   import pandas as pd
-  import cctx
+  import ccxt
   
   exchange = ccxt.okx()  # можно поменять на bybit / binanceus
   ohlcv = exchange.fetch_ohlcv(symbol, timeframe=timeframe, limit=limit)
