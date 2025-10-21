58e212e86a2b993d023bf54d476f257bd3785030
toncho666 <38908277+toncho666@users.noreply.github.com>
2025-10-21 20:40:33 +0300

    Update tg_notification.py

--- tg_notification.py
+++ tg_notification.py
@@ -1,12 +1,14 @@
 import requests
 
-def send_telegram_message(tg_token, tg_chat_id, message: str):
+def send_telegram_message(tg_token, tg_chat_id, message: str, parse_mode: str = "Markdown"):
     """Отправка сообщения в Telegram"""
     if not tg_token or not tg_chat_id:
         print("[WARN] TELEGRAM_TOKEN или TELEGRAM_CHAT_ID не заданы")
         return
     url = f"https://api.telegram.org/bot{tg_token}/sendMessage"
-    payload = {"chat_id": tg_chat_id, "text": message}
+    payload = {"chat_id": tg_chat_id
+              ,"text": message
+              ,"parse_mode": parse_mode}
     try:
         response = requests.post(url, data=payload)
         if response.status_code != 200:
