f71b4f88e506149431dba4f2c225a3173a88df4b
toncho666 <38908277+toncho666@users.noreply.github.com>
2025-11-26 11:18:52 +0300

    Update and rename main.py to load_main.py

---
+++ load_market_data/load_main.py
@@ -0,0 +1,27 @@
+from market_data_fetcher import MarketDataFetcher
+from pg_client import PostgresClient
+import os
+
+def main():
+    symbol = "BTC/USDT"
+    timeframe = "1h"
+
+    # 1. Получение данных
+    fetcher = MarketDataFetcher("binance")
+    df = fetcher.fetch_ohlcv(symbol, timeframe)
+    
+    # 2. Подключение к БД
+    client = PostgresClient(
+        host = os.getenv("DB_HOST"),
+        port = 5432,
+        user = os.getenv("DB_USER"),
+        password = os.getenv("DB_PASS"),
+        database = os.getenv("DB_NAME")
+    )
+
+    # 3. Запись
+    client.save_market_data(df, "btc_usd_t")
+
+
+if name == "__main__":
+    main()
