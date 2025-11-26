8664b645d2b5e752d00bb2984d6671d7635c7f70
toncho666 <38908277+toncho666@users.noreply.github.com>
2025-11-26 11:08:10 +0300

    Update pg_client.py

--- load_market_data/pg_client.py
+++ load_market_data/pg_client.py
@@ -2,15 +2,16 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 class PostgresClient:
-    def __init__(self, host, port, user, password, database):
+    def __init__(self, host, port, user, password, database, tbl_nm):
         conn_str = f"postgresql://{user}:{password}@{host}:{port}/{database}"
         self.engine = create_engine(conn_str, pool_pre_ping=True)
+        self.tbl_nm = tbl_nm
 
     def save_market_data(self, df: pd.DataFrame):
         df.to_sql(
-            "market_data",
+            name=self.tbl_nm,
+            schema="test",
             self.engine,
             if_exists="append",
-            index=False,
-            schema="test"
+            index=False
         )
