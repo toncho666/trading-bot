3ec0a920a77094944a20180715ec6027e793548b
toncho666 <38908277+toncho666@users.noreply.github.com>
2025-11-26 12:11:10 +0300

    Update market_data_fetcher.py

--- load_market_data/market_data_fetcher.py
+++ load_market_data/market_data_fetcher.py
@@ -4,7 +4,8 @@ from datetime import datetime
 
 class MarketDataFetcher:
     def __init__(self, exchange_name="binance"):
-        self.exchange = getattr(ccxt, exchange_name)()
+        # self.exchange = getattr(ccxt, exchange_name)()
+        self.exchange = ccxt.okx()
 
     def fetch_ohlcv(self, symbol: str, timeframe: str = "1h", limit: int = 500) -> pd.DataFrame:
         """Запрос OHLCV данных."""
