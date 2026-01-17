c44b52449f1ba66ff8d44bfaaee0fc53e2bd0bb8
toncho666 <38908277+toncho666@users.noreply.github.com>
2026-01-17 18:59:12 +0300

    Update close_open_1pct.py

--- strategies/close_open_1pct.py
+++ strategies/close_open_1pct.py
@@ -4,7 +4,7 @@ def trading_strategy(df: pd.DataFrame) -> pd.DataFrame:
     """
     Добавляет колонку 'signal' в DataFrame.
     
-    - 1 если Close > Open на threshold (по умолчанию 0.2%)
+    - 1 если Close > Open на threshold (по умолчанию 0.5%)
     - -1 если Close < Open на threshold
     - 0 иначе
     """
@@ -15,7 +15,7 @@ def trading_strategy(df: pd.DataFrame) -> pd.DataFrame:
 
     # логика сигналов
     df["signal"] = 0
-    df.loc[change > 0.002, "signal"] = 1   # buy
-    df.loc[change < -0.002, "signal"] = -1 # sell
+    df.loc[change > 0.005, "signal"] = 1   # buy
+    df.loc[change < -0.005, "signal"] = -1 # sell
 
     return df
