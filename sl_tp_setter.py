a284a6d6ab4c1f34eec02a47e9ba736e7232a6d3
toncho666 <38908277+toncho666@users.noreply.github.com>
2026-02-28 18:52:38 +0300

    Update sl_tp_setter.py

--- sl_tp_setter.py
+++ sl_tp_setter.py
@@ -47,7 +47,7 @@ def get_sl_tp_val(strategy_name: str
         "close_open_1pct": {"sl": 0.006,  "tp": 0.035},
         "close_open_engulfing": {"sl": 0.011,  "tp": 0.035},
         # "macd": {"sl": 0.01,  "tp": 0.025},
-        "candles": {"sl": 0.01,  "tp": 0.05},
+        "candles": {"sl": 0.008,  "tp": 0.04},
         # ← здесь можно добавить новые стратегии
     }
 
