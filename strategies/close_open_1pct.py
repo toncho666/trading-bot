150173a98b01a7e688b6f307679b3abc70cd8331
toncho666 <38908277+toncho666@users.noreply.github.com>
2026-02-28 18:50:37 +0300

    Update close_open_1pct.py

--- strategies/close_open_1pct.py
+++ strategies/close_open_1pct.py
@@ -2,6 +2,9 @@ import pandas as pd
 
 def trading_strategy(df: pd.DataFrame) -> pd.DataFrame:
     """
+    change_perc = 0.5
+    stop_loss = 0.6
+    take_profit = 3.5
     Добавляет колонку 'signal' в DataFrame.
     
     - 1 если Close > Open на threshold (по умолчанию 0.5%)
