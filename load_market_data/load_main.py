1d8cdc1687b2e04b6b55df9a4260461bde58ee16
toncho666 <38908277+toncho666@users.noreply.github.com>
2026-05-16 19:14:04 +0300

    Fix syntax error in database connection code

--- load_market_data/load_main.py
+++ load_market_data/load_main.py
@@ -19,7 +19,7 @@ def main():
     # 2. Подключение к БД
     client = PostgresClient(
         host = os.getenv("DB_HOST"),
-        port = os.getenv("DB_PORT),
+        port = os.getenv("DB_PORT"),
         user = os.getenv("DB_USER"),
         password = os.getenv("DB_PASS"),
         database = os.getenv("DB_NAME")
