1c793cf8e32e455b0531994e12cdc0b04b826008
toncho666 <38908277+toncho666@users.noreply.github.com>
2026-05-16 18:28:28 +0300

    Add DB_PORT environment variable configuration

--- runner.py
+++ runner.py
@@ -16,6 +16,7 @@ import pandas as pd
 # 1. Конфигурация окружения
 # ============================================================
 DB_HOST = os.getenv("DB_HOST")
+DB_PORT= os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
