62dc8f00ef32126bc7960d2e506a748fe52673d3
toncho666 <38908277+toncho666@users.noreply.github.com>
2026-07-11 18:39:45 +0300

    Update runner.py

--- runner.py
+++ runner.py
@@ -97,7 +97,7 @@ def run_strategy(file):
 
         
         strategies = {
-                "close_open_1pct": {"sl": 0.006,  "tp": 0.035},
+                "close_open_1pct": {"sl": 0.006,  "tp": 0.025},
                 "close_open_engulfing": {"sl": 0.011,  "tp": 0.035},
                 "macd_hist": {"sl": 0.008,  "tp": 0.035},
                 "candles": {"sl": 0.008,  "tp": 0.04},
